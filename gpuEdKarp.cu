ba6707c680772301a98f9b27918158b4d11cd94d
Roberto <rmstobi@gmail.com>
2020-08-09 21:02:07 -0500

    remove unused

--- gpuEdKarp.cu
+++ gpuEdKarp.cu
@@ -5,7 +5,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "sequential.h"
 
 #define IDX(i, j, n) ((i) * (n) + (j))
 /*
--- gpuEdKarp.h
+++ gpuEdKarp.h
@@ -1,5 +1,3 @@
 #include "graphs.h"
 
 Flow *edKarpGpu(Graph *g, int s, int t);
-
-Flow *dinicGpu(Graph *g, int s, int t);
