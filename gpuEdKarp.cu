fa2c4f6ffbd7c86dfb65cb1e0fd98707e6a09b4b
Roberto <rmstobi@gmail.com>
2020-08-12 19:36:47 -0500

    fix

--- gpuEdKarp.cu
+++ gpuEdKarp.cu
@@ -45,7 +45,7 @@ int BFS(Graph *g, int *flowMatrix, int *parents, int *pathCapacities, int s, int
       if (u == v){
         continue;
       }
-      printf("Loop U %d , V %d\n"u,v);
+      printf("Loop U %d , V %d\n",u,v);
       printf("flowmatrix bfs %d\n",flowMatrix[IDX(u, v, g->n)]);
       printf("cap bfs %d\n",g->capacities[IDX(u, v, g->n)]);
       int residual = g->capacities[IDX(u, v, g->n)] - flowMatrix[IDX(u, v, g->n)];
@@ -62,7 +62,7 @@ int BFS(Graph *g, int *flowMatrix, int *parents, int *pathCapacities, int s, int
       }
     }
   }
-  printf("pathCap %d\n"pathCapacities[t]);
+  printf("pathCap %d\n",pathCapacities[t]);
   return 0;
 }
 
